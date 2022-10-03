e51fbe246021909b7e5571c4adba416c20fa5c9c
Naveed Hassan <08es34@gmail.com>
2022-10-03 13:31:13 +0500

    added seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +16,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::create([
+            'name' => 'Naveed',
+            'email' => '08es34@gmail.com',
+            'password' => bcrypt('123456'),
+            'email_verified_at' => Carbon::now(),
+        ]);
     }
 }
