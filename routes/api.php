d397eb2c5bcd4f64bae9d17afeb2221f5d2d2589
Naveed Hassan <08es34@gmail.com>
2022-10-27 14:55:07 +0500

    * cleanup

--- routes/api.php
+++ routes/api.php
@@ -5,17 +5,6 @@ use App\Http\Controllers\SitemapController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 require __DIR__ . '/auth.php';
 
 Route::middleware(['auth:sanctum'])->group(function () {
